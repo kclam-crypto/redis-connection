7e0cc727266b1b9b43a55d5dc50355623d997a16
kc.lam@crypto.com <kc.lam@crypto.com>
2021-04-08 09:09:37 +0800

    handle no cert and remove info command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
@@ -14,15 +15,17 @@ func main() {
 	password := os.Args[3]
 	now := time.Now().Format(time.RFC3339)
 	fmt.Printf("Connect to %s as %s at %s\n", url, username, now)
-	c, err := redis.Dial(
-		"tcp", url+":6379",
-		redis.DialUseTLS(true),
-		redis.DialUsername(username),
-		redis.DialPassword(password),
-	)
+	c, err := redis.Dial("tcp", url+":6379", redis.DialUseTLS(true), redis.DialUsername(username), redis.DialPassword(password))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fmt.Println(err.Error())
+		if strings.Contains(err.Error(), "x509: certificate is valid") {
+			fmt.Println("Skip cert verfication")
+			c, err = redis.Dial("tcp", url+":6379", redis.DialUseTLS(true), redis.DialTLSSkipVerify(true), redis.DialUsername(username), redis.DialPassword(password))
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+		}
 	}
 	defer c.Close()
 	fmt.Printf("SET a: %s\n", now)
@@ -31,13 +34,6 @@ func main() {
 	}
 	fmt.Print("GET a: ")
 	v, err := c.Do("GET", "a")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("%s\n", string(v.([]byte)))
-	}
-	fmt.Print("INFO")
-	v, err = c.Do("INFO")
 	if err != nil {
 		fmt.Println(err)
 	} else {
